d566ec14052ba2195cc72ef5efb0539f2fb8a453
RepublicOfLinux <153024339+RepublicOfLinux@users.noreply.github.com>
2024-01-08 23:12:27 +0100

    Create universal-zsh-install-script.sh
    Zsh install script for any Linux platform including iso and android (ish for iOS and termux for android)

---
+++ universal-zsh-install-script.sh
@@ -0,0 +1,7 @@
+su
+apk add zsh
+sudo apt install zsh
+sudo pacman -S zsh
+yum install zsh
+sudo zypper zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

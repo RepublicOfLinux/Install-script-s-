698475605bfc0ecbdffa4efbfd0ff06b5e17d383
RepublicOfLinux <153024339+RepublicOfLinux@users.noreply.github.com>
2024-01-08 23:03:15 +0100

    Create Ish-install.sh
    Here is a simple and basic install script for ish shell (note: IOS only)

---
+++ Ish-install.sh
@@ -0,0 +1,8 @@
+apk add neofetch
+apk add git
+apk add zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+apk add figlet 
+figlet "Alpine Linux"
+neofetch
+figlet "Done"
